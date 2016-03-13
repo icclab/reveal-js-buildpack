6a173546974dbe7b716c925436faeb926f65ea70
Christof Marti <mach@zhaw.ch>
2016-03-13 19:24:29 +0100

    first version of revealjs buildpack

--- lib/installnode.sh
+++ lib/installnode.sh
@@ -15,8 +15,12 @@ install_nodejs() {
 install_node_modules() {
   local build_dir=${1:-}
 
+  echo "$build_dir"
+  ls $build_dir/
+  
   if [ -e $build_dir/package.json ]; then
     cd $build_dir
+
     echo "Pruning any extraneous modules"
     npm prune --unsafe-perm --userconfig $build_dir/.npmrc 2>&1
     if [ -e $build_dir/npm-shrinkwrap.json ]; then
