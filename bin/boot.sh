6a1bbb6f734d42e72a4e89fd82b909c8a3090575
Christof Marti <mach@zhaw.ch>
2016-03-14 03:11:01 +0100

    first version of revealjs buildpack

--- bin/boot.sh
+++ bin/boot.sh
@@ -2,4 +2,5 @@ export NODE_HOME="$HOME/vendor/node"
 export PATH="$NODE_HOME/bin:$PATH:$HOME/bin:$HOME/node_modules/.bin"
 export NODE_ENV=${NODE_ENV:-production}
 
-exec grunt serve --hostname 0.0.0.0 --port $PORT 
+echo "Starting Server on port $PORT"
+grunt serve --hostname 0.0.0.0 --port $PORT 
