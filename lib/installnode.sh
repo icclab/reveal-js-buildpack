f90c3e92b71fc0ffb552121c03cd8fae74dc31f5
Christof Marti <mach@zhaw.ch>
2016-03-14 11:44:31 +0100

    removed debug messages in installnodes.sh

--- lib/installnode.sh
+++ lib/installnode.sh
@@ -15,12 +15,8 @@ install_nodejs() {
 install_node_modules() {
   local build_dir=${1:-}
 
-  echo "$build_dir"
-  ls $build_dir/
-  echo "PATH: $PATH"
   if [ -e $build_dir/package.json ]; then
     cd $build_dir
-
     echo "Pruning any extraneous modules"
     npm prune --unsafe-perm --userconfig $build_dir/.npmrc 2>&1
     if [ -e $build_dir/npm-shrinkwrap.json ]; then
