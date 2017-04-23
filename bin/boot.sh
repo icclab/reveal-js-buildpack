11692b12b80b230e1883b7012870a9512d4706aa
Christof Marti <mach@zhaw.ch>
2017-04-23 10:16:32 +0200

    fixed startup parameters for grunt

--- bin/boot.sh
+++ bin/boot.sh
@@ -3,4 +3,4 @@ export PATH="$NODE_HOME/bin:$PATH:$HOME/bin:$HOME/node_modules/.bin"
 export NODE_ENV=${NODE_ENV:-production}
 	
 echo "Starting Server on port $PORT"
-grunt serve --hostname 0.0.0.0 --port $PORT 
+grunt serve --hostname='0.0.0.0' --port=$PORT 
