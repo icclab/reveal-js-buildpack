53f1c9cfcc81e8e2b92e222cdd49fc6c152f3700
Christof Marti <mach@zhaw.ch>
2019-04-12 16:20:59 +0200

    Fixed for version 3.8.0 of reveal.js

--- bin/boot.sh
+++ bin/boot.sh
@@ -3,4 +3,7 @@ export PATH="$NODE_HOME/bin:$PATH:$HOME/bin:$HOME/node_modules/.bin"
 export NODE_ENV=${NODE_ENV:-production}
 	
 echo "Starting Server on port $PORT"
-grunt serve --hostname='0.0.0.0' --port=$PORT 
+export GRUNT_PORT=${PORT:8080}
+export GRUNT_BROWSER=false
+npm start
+#grunt serve --hostname='0.0.0.0' --port=$PORT 
