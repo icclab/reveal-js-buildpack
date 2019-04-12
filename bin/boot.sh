813c535ab93903029f17767417c62603780b8c66
Christof Marti <mach@zhaw.ch>
2019-04-12 16:31:41 +0200

    Fix a substitution error in boot.sh

--- bin/boot.sh
+++ bin/boot.sh
@@ -1,9 +1,8 @@
 export NODE_HOME="$HOME/vendor/node"
 export PATH="$NODE_HOME/bin:$PATH:$HOME/bin:$HOME/node_modules/.bin"
 export NODE_ENV=${NODE_ENV:-production}
-	
-echo "Starting Server on port $PORT"
-export GRUNT_PORT=${PORT:8080}
+export GRUNT_PORT=${PORT:-8080}	
 export GRUNT_BROWSER=false
+echo "Starting Server on port $GRUNT_PORT"
 npm start
 #grunt serve --hostname='0.0.0.0' --port=$PORT 
